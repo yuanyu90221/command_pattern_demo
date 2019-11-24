af27bba4fcda2c9a780ed4332ca74265f71da020
json <yuanyu90221@gmail.com>
2019-11-24 22:59:00 +0800

    init

---
+++ client.js
@@ -0,0 +1,14 @@
+const Invoker = require('./invoker');
+const createSendStatusCmd = require('./command');
+const statusUpdateService = require('./target');
+
+const invoker = new Invoker();
+const command = createSendStatusCmd(statusUpdateService, 'Hi!');
+
+function test() {
+    invoker.run(command);
+    invoker.undo();
+    invoker.delay(command, 1000*2);
+}
+
+test();
